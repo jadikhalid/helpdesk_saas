6eb005e68661b936f18a799b3ac48fef8471ddf4
Jadi Khalid <jadikhalid@gmail.com>
2026-03-05 00:53:38 +0000

    ok

--- config/db.php
+++ config/db.php
@@ -1,10 +1,17 @@
 <?php
 // config/db.php
 
+use Dotenv\Dotenv;
+
+require_once __DIR__ . '/../vendor/autoload.php'; // Si vous utilisez Composer pour gérer les dépendances
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 $host = 'localhost';
 $db   = 'helpdesk_saas';
 $user = 'root';
-$pass = 'malakjadi'; // Celui que vous avez défini tout à l'heure
+$pass = $_ENV['DB_PASSWORD']; // Celui que vous avez défini tout à l'heure
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
